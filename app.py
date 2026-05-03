4e626a6ddb249d086a4360121c8bee8221f852af
nandanajp1105 <nandanajp1105@gmail.com>
2026-05-03 16:18:45 +0530

    Render deployment fix

--- app.py
+++ app.py
@@ -87,7 +87,8 @@ def best_time():
 
 # -------- RUN --------
 if __name__ == '__main__':
-    import os
+   import os
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
